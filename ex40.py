b0320e9f85804b5882fb06476de51e0d48fb84ee
Yangyi <yangyionly@qq.com>
2020-03-29 19:23:39 +0800

    update ex40.py

--- ex40.py
+++ ex40.py
@@ -2,7 +2,7 @@ class Song(object):
 
     def __init__(self, lyrics):
         self.lyrics = lyrics
-
+    a = 123
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(line)
@@ -15,5 +15,9 @@ bulls_on_parade = Song(["They rally around the family",
                         "With pockets full of shells"])
 
 happy_bday.sing_me_a_song()
+print(happy_bday.a)
+
+Song.a = 345
 
 bulls_on_parade.sing_me_a_song()
+print(happy_bday.a)
